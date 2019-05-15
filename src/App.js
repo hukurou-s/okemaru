7e7cfd60b86667679fb584485e8de5c46cc10864
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2019-05-15 16:29:34 +0900

    react-routerを使って指導側と生徒側のページを分割

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Classroom from './components/Classroom';
+import Classroom from './components/admin/Classroom';
+import StatusButton from './components/student/StatusButton';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function App() {
     return (
+        <BrowserRouter>
             <div>
-            <Classroom />
+            <Route exact path='/' render={() => <StatusButton />} />
+            <Route path='/admin' render={() => <Classroom />} />
             </div>
+            </BrowserRouter>
     );
 }
 
--- src/components/admin/Classroom.js
+++ src/components/admin/Classroom.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Table from './Table';
 
 class Classroom extends React.Component {
+
+    constructor() {
+        super();
+    }
+
     rederTable(name, state) {
         return <Table name={name} state={state} />;
     }
@@ -9,6 +14,7 @@ class Classroom extends React.Component {
     render () {
         return (
                 <div>
+                <h1>{"Admin Page"}</h1>
                 {this.rederTable("H01", "OK")}
             {this.rederTable("G01", "NG")}
             {this.rederTable("H02", "NG")}
